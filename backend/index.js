b159b145f25983c57c010970792df8199c761b24
Lev Feldman <feldmanlev@yahoo.com.au>
2022-06-13 18:21:15 +1000

    Latest Changes

--- backend/index.js
+++ backend/index.js
@@ -2,66 +2,69 @@ const { request } = require("express")
 const express = require("express")
 const res = require("express/lib/response")
 const app = express()
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+const games = [
+    {
+      "id": 1,
+      "title": "Valorant",
+      "image": "images/Valorant.jpg",
+      "avatar": true
+    },
+    {
+      "id": 2,
+      "title": "Fortnite",
+      "image": "images/Fortnite.jpg",
+      "avatar": false
+    },
+    {
+      "id": 3,
+      "title": "Apex Legends",
+      "image": "images/Apex-Legends.jpg",
+      "avatar": true
+    }
+  ]
+
 
 const idGenerator = ()=> {
-  let maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  let maxId = games.length > 0 ? Math.max(...games.map(n => n.id)) : 0
   return maxId ++
 }
 
 // // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
+//   response.end(JSON.stringify(games))
 // })
 app.get("/", (request, response)=> {
     response.send("<h1>hello i am alive</h1>")
 })
-app.get("/api/notes", (request, response)=>{
-    response.json(notes)
+app.get("/api/games", (request, response)=>{
+    response.json(games)
 })
 app.get("/test", (request, response)=> {
     response.send("<h1>I am working</h1>")
 })
-app.get("/api/notes/:id", (request, response)=>{
+app.get("/api/games/:id", (request, response)=>{
   const id = Number(request.params.id)
 
-  const note = notes.find(note=> note.id === id )
+  const note = games.find(note=> note.id === id )
   if(note) {
     response.json(note)
   }else {
     response.status(404).end()
   }
 })
-app.delete("/api/notes/:id", (request, response)=> {
+app.delete("/api/games/:id", (request, response)=> {
   const id = Number(request.params.id)
 
-  notes = notes.filter((note)=> note.id !== id)
+  games = games.filter((note)=> note.id !== id)
   response.status(204).end()
 })
 
-app.post("/api/notes", (request, response)=> {
+app.post("/api/games", (request, response)=> {
 
   const body = request.body
 
@@ -76,12 +79,23 @@ app.post("/api/notes", (request, response)=> {
     important: body.important || false
   }
 
-  notes = notes.concat(note)
+  games = games.concat(note)
 
   response.json(note)
 })  
 
-const PORT = 3001
+
+// const requestLog = (request, response, next)=> {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+
+// app.use(requestLog())
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
 })
